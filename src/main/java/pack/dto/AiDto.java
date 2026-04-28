c5c57dd4ef96035f375a11af1de42f50e1d10b2e
2021301041-crypto <2021301041@skuniv.ac.kr>
2026-04-28 16:36:22 +0900

    AI 클라이언트 및 유틸리티 클래스 추가

---
+++ src/main/java/pack/dto/AiDto.java
@@ -0,0 +1,43 @@
+package pack.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+public class AiDto {
+
+    @Getter
+    @AllArgsConstructor
+    public static class Request {
+        private String query;
+        private int top_k;
+        private String mode;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    public static class Response {
+        private String answer;
+        private List<RecipeResult> results;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    public static class RecipeResult {
+        private int rank;
+        @JsonProperty("recipe_id")
+        private int recipeId;
+        private String name;
+        private String category;
+        @JsonProperty("cooking_way")
+        private String cookingWay;
+        private double score;
+        @JsonProperty("image_url")
+        private String imageUrl;
+        @JsonProperty("thumbnail_url")
+        private String thumbnailUrl;
+    }
+}
